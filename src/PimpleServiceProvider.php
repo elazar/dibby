498ef11365283d9cf951c77225ec5be086383dfb
Matthew Turland <tobias382@gmail.com>
2021-11-13 09:10:04 -0600

    Tidy up dependencies a bit

--- src/PimpleServiceProvider.php
+++ src/PimpleServiceProvider.php
@@ -10,6 +10,7 @@ use League\Route\Strategy\ApplicationStrategy;
 use League\Route\Strategy\StrategyInterface;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
+use Nyholm\Psr7Server\ServerRequestCreatorInterface;
 use Pimple\Container;
 use Pimple\Psr11\Container as PsrContainer;
 use Pimple\ServiceProviderInterface;
@@ -25,6 +26,7 @@ class PimpleServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $pimple)
     {
+        // PSR-7 implementation
         $pimple[Psr17Factory::class] = fn() => new Psr17Factory;
         $pimple[ResponseFactoryInterface::class] = fn($c) => $c[Psr17Factory::class];
         $pimple[ServerRequestFactoryInterface::class] = fn($c) => $c[Psr17Factory::class];
@@ -32,17 +34,17 @@ class PimpleServiceProvider implements ServiceProviderInterface
         $pimple[UploadedFileFactoryInterface::class] = fn($c) => $c[Psr17Factory::class];
         $pimple[StreamFactoryInterface::class] = fn($c) => $c[Psr17Factory::class];
 
+        // PSR-7 request creator
         $pimple[ServerRequestCreator::class] = fn($c) => new ServerRequestCreator(
             $c[ServerRequestFactoryInterface::class],
             $c[UriFactoryInterface::class],
             $c[UploadedFileFactoryInterface::class],
             $c[StreamFactoryInterface::class],
         );
-        $pimple[ServerRequestInterface::class] = fn($c) => $c[ServerRequestCreator::class]->fromGlobals();
+        $pimple[ServerRequestCreatorInterface::class] = fn($c) => $c[ServerRequestCreator::class];
+        $pimple[ServerRequestInterface::class] = fn($c) => $c[ServerRequestCreatorInterface::class]->fromGlobals();
 
-        $pimple[EmitterInterface::class] = fn($c) => $c[SapiEmitter::class];
-        $pimple[SapiEmitter::class] = fn() => new SapiEmitter;
-
+        // PSR-15 request handler implementation
         $pimple[ApplicationStrategy::class] = fn($c) => (new ApplicationStrategy)
             ->setContainer(new PsrContainer($c));
         $pimple[StrategyInterface::class] = fn($c) => $c[ApplicationStrategy::class];
@@ -51,12 +53,18 @@ class PimpleServiceProvider implements ServiceProviderInterface
         );
         $pimple[RequestHandlerInterface::class] = fn($c) => $c[Router::class];
 
+        // Response emitter
+        $pimple[EmitterInterface::class] = fn($c) => $c[SapiEmitter::class];
+        $pimple[SapiEmitter::class] = fn() => new SapiEmitter;
+
+        // Application
         $pimple[Application::class] = fn($c) => new Application(
             $c[ServerRequestInterface::class],
             $c[RequestHandlerInterface::class],
             $c[EmitterInterface::class],
         );
 
+        // Controllers
         $pimple[IndexController::class] = fn($c) => new IndexController(
             $c[ResponseFactoryInterface::class],
         );
