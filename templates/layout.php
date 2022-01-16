1af7c4ffee8b5f4b60f8182b92462dfb595fd13d
Matthew Turland <tobias382@gmail.com>
2022-01-16 12:10:53 -0600

    Add favicon and A2HS support
    See:
    * https://developer.mozilla.org/en-US/docs/Glossary/Favicon
    * https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen
    * https://favicon.io/favicon-generator/

--- templates/layout.php
+++ templates/layout.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="/pico.min.css">
   <link rel="stylesheet" href="/custom.css">
+  <link rel="manifest" href="/manifest.webmanifest">
   <script src="/custom.js"></script>
 </head>
 <body>
