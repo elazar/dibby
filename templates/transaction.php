3db39b8a9b972ac67b7b5b2b7018e1917afd95da
Matthew Turland <tobias382@gmail.com>
2022-03-13 16:39:20 -0500

    Remove :w left in transaction template

--- templates/transaction.php
+++ templates/transaction.php
@@ -33,7 +33,6 @@ $title = (isset($id) ? 'Edit' : 'Add') . ' Transaction';
 
   <label for="amount">Amount</label>
   <input id="amount" name="amount" type="number" value="<?= $this->e(isset($amount) ? number_format($amount, 2) : '') ?>" step="0.01" required autofocus placeholder="e.g. 1.99">
-:w
 
   <?php $this->insert('accounts-datalist', ['accounts' => $accounts]); ?>
 
